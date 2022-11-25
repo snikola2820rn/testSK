b0bb3924bae0653e4b1035f74653dc989c4c1699
snikoa2820rn <snikola2820rn@raf.rs>
2022-11-25 12:56:37 +0100

    `Pocetak`

---
+++ src/main/java/test/TestApp.java
@@ -0,0 +1,32 @@
+package test;
+
+import spec.Spec;
+import spec.StorageManager;
+
+import java.util.Scanner;
+
+public class TestApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        if(args.length < 1)
+        {
+            System.out.println("Missing storage path.");
+            System.exit(0);
+        }
+
+        try
+        {
+            Class.forName("test");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        String path = args[0];
+
+        Spec st = StorageManager.getStorage("impl.DriveImplementation");
+
+    }
+}
