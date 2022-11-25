bd1296d19330af52dc0d2757b0ea078a7b80de09
JanaMarkovic12621rn <jmarkovic12621rn@raf.rs>
2022-11-25 13:02:25 +0100

    komit

--- src/main/java/test/TestApp.java
+++ src/main/java/test/TestApp.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class TestApp {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        System.out.println("KOMITTTTTTT");
         if(args.length < 1)
         {
             System.out.println("Missing storage path.");
